a86a49cd4ac7948e9d0490b4b010bc6b30a0bc40
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-03-10 21:00:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firstapps/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.firstapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var txt = findViewById<TextView>(R.id.txt)
+
+        txt.text = "HALLO TIGA HARI LALU TANGGAL LIVERPOOL VS MU MARET 2023"
+
+        var img = findViewById<ImageView>(R.id.img)
+
+        img.setOnClickListener {
+            Toast.makeText(this,"JANGAN NANGIS", Toast.LENGTH_SHORT)
+            Log.d("AKSI","Telah ditekan cuy")
+        }
+
+    }
+}
